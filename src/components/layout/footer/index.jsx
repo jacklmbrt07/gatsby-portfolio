499d920bc3e22e998616b03a6b876c69fa99f00d
jacklmbrt07 <jacklmbrt@gmail.com>
2020-09-28 10:21:22 -0700

    readme

--- src/components/layout/footer/index.jsx
+++ src/components/layout/footer/index.jsx
@@ -1,33 +1,45 @@
-import React from 'react';
-import socialMedia from "../../../data/socialMedia.json";
-import data from "./../../../data/data";
-import { FooterStyle, FooterBody, SubRight, CopyRight,  } from './style'
-import {ContainerLayout, ButtonDefault} from '../../common'
+import React from "react"
+import socialMedia from "../../../data/socialMedia.json"
+import data from "./../../../data/data"
+import { FooterStyle, FooterBody, SubRight, CopyRight } from "./style"
+import { ContainerLayout, ButtonDefault } from "../../common"
 
 const Footer = () => {
-	return (
-		<>
-			<FooterStyle>
-				<ContainerLayout>
-					<FooterBody>
-						<div>
-							<p className="text-primary quote"> Ready to take the next step and work together? </p>
-							<ButtonDefault href={`mailto:${data.SiteContact.email}`}> Contact me </ButtonDefault>
-						</div>
-					</FooterBody>
-					<div className="box">
-						<SubRight>Compare yourself to who you were yesterday, not to who someone else is today.</SubRight>
-						<CopyRight className="text-dark">
-							© 
-							<span> {new Date().getFullYear()}, Built with {` `} 
-								<a href="https://www.gatsbyjs.org">Gatsby</a>{" "}
-          		</span> 
-							Copyright 2020 by {data.SiteAuthor} </CopyRight>
-					</div>
-				</ContainerLayout>
-			</FooterStyle>
-		</>
-	)
+  return (
+    <>
+      <FooterStyle>
+        <ContainerLayout>
+          <FooterBody>
+            <div>
+              <p className="text-primary quote">
+                {" "}
+                Ready to take the next step and work together?{" "}
+              </p>
+              <ButtonDefault href={`mailto:${data.SiteContact.email}`}>
+                {" "}
+                Contact me{" "}
+              </ButtonDefault>
+            </div>
+          </FooterBody>
+          <div className="box">
+            <SubRight>
+              Compare yourself to who you were yesterday, not to who someone
+              else is today.
+            </SubRight>
+            <CopyRight className="text-dark">
+              ©
+              <span>
+                {" "}
+                {new Date().getFullYear()}, Built with {` `}
+                <a href="https://www.gatsbyjs.org">Gatsby</a>{" "}
+              </span>
+              Copyright 2020 by {data.SiteAuthor}{" "}
+            </CopyRight>
+          </div>
+        </ContainerLayout>
+      </FooterStyle>
+    </>
+  )
 }
 
-export default Footer;
+export default Footer
