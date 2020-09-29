584f4d078f1c649021ed5e5db2f60d58cf0ca5d2
jacklmbrt07 <jacklmbrt@gmail.com>
2020-09-29 11:59:07 -0700

    updated About

--- README.md
+++ README.md
@@ -4,14 +4,14 @@ Portfolio To-Do:
 - ~~add space between coffee~~
 - ~~move icons next navbar links~~
 - ~~remove twitter link after Avatar~~
-- change about me
-- change button format for 'download resume' too big
-- add resume, VisualCV
-- remove 'contact me section'
+- ~~change about me~~
+- ~~change button format for 'download resume' too big~~
+- ~~add resume, VisualCV~~
+- ~~remove 'contact me section'~~
 - make 'about me' text smaller
-- ' good design doesnt date' remove
+- ~~' good design doesnt date' remove~~
 - add another section under about me
-- add descriptions to github depositories
+- ~~add descriptions to github depositories~~
 - add portfolio items
 - add blog items
 - portrait is pixelated
--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -67,30 +67,46 @@ const About = () => {
               </Text>
               <Text>
                 {" "}
-                After a stint as a Construction Engineer, I became so fascinated
-                with computer programming and made the difficult yet ruminated
-                decision to switch careers. During that stint, I worked at the
-                Oroville-Thermalito Dam complex in Northern California where I
-                picked up some of the hardest lessons in project management, and
-                leadership. Web-development has allowed me to utilize my problem
-                solving skills, while simultaneously allowing me to let my
-                creative side flow freely. I enjoy problem solving, and use my
-                hyper focus on attention to detail to create functional yet
-                minimalistic designs. I would like to utilize my skills in the
-                tech world to lead a team with a destination that is both
-                ethical and economic.
+                In a previous life, which seemed like it was decades ago, I was
+                working as a Construction Engineer making trips around San
+                Francisco and Oakland, and ultimately ended up at the
+                Oroville-Thermalito Dam complex a couple hundred miles up north.
+                During that time, I picked up some of the hardest yet beneficial
+                lessons in project management and leadership. Despite the
+                success I experienced there (after countless failures), I became
+                fascinated by computer programming, and after some careful
+                thought, decided to switch careers.
+              </Text>
+              <Text>
+                {" "}
+                My favorite thing about programming is that there is a quite
+                literally, an infinite supply of things to learn, and problems
+                to solve. Web-development has allowed me to let my creative side
+                flow freely, and use my focused attention to detail. Maybe I
+                binged watched too much of HBO’s <em>Silicon Valley</em>, but I
+                picture myself one day leading a team creating something that is
+                both ethical and economic.
+              </Text>
+              <Text>
+                {" "}
+                Like every instagram user in this decade, I love to travel. I
+                like to pretend I’m Anthony Bourdain, but in reality I’m taking
+                selfies with a 12 foot long selfie stick in front of the Eiffel
+                Tower, followed by an authenitc meal at the Golden Arches for
+                lunch, just like every one else! I’m only joking, and I have
+                indeed spent many months overseas in Australia, Southeast Asia,
+                and other destinations (Australian is my second language). While
+                abroad, I picked up photography as a hobby (and drastically
+                reduced my selfie stick length). I treat my photographs, vinyl
+                records, and books like my step-children. I enjoy all things
+                involving art, history, food, and could spend hours in
+                philosophical discussions.
               </Text>
               <Text>
                 {" "}
                 I graduated with a B.S. degree of Civil Engineering in 2017 from
-                San Jose State University. I love traveling and have spent much
-                time abroad in Australia, Southeast Asia, and other
-                destinations. Whilst traveling I picked up photography as a
-                hobby. I treat my photographs, vinyl records, and books like my
-                children. I enjoy all things art, history, San Franciscos and
-                Oaklands excellent food and drink establishments, and could
-                spend hours in philosophical discussions with no winner or
-                loser.
+                San Jose State University, and completed the Software
+                Engineering Immersive Program from General Assembly in 2020.{" "}
               </Text>
               <ResumeButton href="resume.pdf" target="_blank">
                 {" "}
