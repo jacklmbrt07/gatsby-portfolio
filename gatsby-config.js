388821a3076d85c84525a94ebac8eacfe3a1cfc2
jacklmbrt07 <jacklmbrt@gmail.com>
2020-10-07 12:28:09 -0700

    successfully made image filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,7 +39,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `photography`,
-        path: `${__dirname}/src/images/photography`,
+        path: `${__dirname}/src/data/photography`,
       },
     },
     {
