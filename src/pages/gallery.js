fd52213d970aa8d3c833a0336157603edec43906
jacklmbrt07 <jacklmbrt@gmail.com>
2020-10-07 13:06:05 -0700

    successfully pulled images

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,13 +1,62 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-// import { StaticQuery, graphql } from "gatsby"
-// import Img from "gatsby-image"
+import { StaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import { Maximize } from "react-feather"
 
-const GalleryPage = ({ data }) => {
+const GalleryPage = () => {
   return (
     <Layout>
       <SEO title="Gallery" />
+      <StaticQuery
+        query={graphql`
+          {
+            allFile(
+              filter: {
+                extension: { regex: "/(jpg)|(jpeg)|(png)/" }
+                relativeDirectory: { eq: "australia" }
+              }
+            ) {
+              edges {
+                node {
+                  id
+                  name
+                  relativePath
+                  birthTime
+                  childImageSharp {
+                    fluid {
+                      base64
+                      tracedSVG
+                      srcWebp
+                      srcSetWebp
+                      originalImg
+                      originalName
+                      aspectRatio
+                      src
+                      srcSet
+                      sizes
+                      presentationWidth
+                      presentationHeight
+                    }
+                  }
+                }
+              }
+            }
+          }
+        `}
+        render={data => (
+          <div class="photography">
+            {data.allFile.edges.map(edge => (
+              <Img
+                fluid={edge.node.childImageSharp.fluid}
+                alt={edge.node.name}
+                style={{ maxWidth: 300 }}
+              />
+            ))}
+          </div>
+        )}
+      />
       Hello World
     </Layout>
   )
